ac662316e4f2c5d90f4e0dcb48d6c979a000dd74
krishnapriyapv <kpriyapv20@gmail.com>
2026-04-23 00:14:48 +0530

    initial commit

--- kreatao_hrplus/hooks.py
+++ kreatao_hrplus/hooks.py
@@ -250,3 +250,27 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
+fixtures = [
+    {
+    "doctype": "Custom Field",
+        "filters": {
+            "module": ["in", ["kreatao_hrplus"]]
+            }
+    },
+    {
+    	"doctype": "Custom Field",
+        "filters": [
+            [
+                "name",
+                "in",
+                [ 
+                    "Shift Type-custom_split_shift",
+                    "Shift Type-custom_break_start_time",
+                    "Shift Type-custom_break_end_time"
+                    
+                 ]
+            ]
+        ]
+    }
+]
